44b80d8a26e984fae45c1db7e05f79ed36311db3
John Mount <jmount@win-vector.com>
2017-01-28 12:48:01 -0800

    add package help

---
+++ R/sigr.R
@@ -0,0 +1,15 @@
+#' sigr: Format Significance Summaries for Reports
+#'
+#' Succinctly format significance summaries of
+#' various models and tests.  The main purpose is unified reporting and planning
+#' of experimental results, working around issue such as the difficulty of
+#' extracting model summary facts (such as with 'lm'/'glm').  This package also
+#' includes empirical tests, such as bootstrap estimates.
+#'
+
+#' To learn more about replyr, please start with the vignette:
+#' \code{vignette('sigrFormatting','sigr')}
+#'
+#' @docType package
+#' @name sigr
+NULL
