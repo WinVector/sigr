1038a43358b88c11810bea1eb654fbea4332d9ca
John Mount <jmount@win-vector.com>
2017-05-05 13:01:52 -0700

    improve docs

--- R/sigr.R
+++ R/sigr.R
@@ -1,7 +1,7 @@
 #' sigr: Format Significance Summaries for Reports
 #'
 #' Succinctly format significance summaries of
-#' various models and tests.  The main purpose is unified reporting and planning
+#' various models and tests (F-test, Chi-Sq-test, Fisher-test, T-test, and rank-significance).  The main purpose is unified reporting and planning
 #' of experimental results, working around issue such as the difficulty of
 #' extracting model summary facts (such as with 'lm'/'glm').  This package also
 #' includes empirical tests, such as bootstrap estimates.
